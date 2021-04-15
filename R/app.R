802883a408e9308c794ec38ecb1864185881af3f
Matt Nethery <mnethery@Matts-MBP.attlocal.net>
2021-04-15 18:19:48 -0400

    Updating dependency loading

--- R/app.R
+++ R/app.R
@@ -6,6 +6,9 @@
 #' @export
 launchApp <- function() {
 
+  ## Load required packages ##
+  CRISPRclassify:::loadRequires()
+
   ## Load HC vars ##
   wrkDir <- CRISPRclassify:::getWrkDir()
   xg_matrix_format_file <- CRISPRclassify:::getMatrixFormatFile(wrkDir)
@@ -186,6 +189,9 @@ server <- function(input, output){
 #' @export
 classifyRepeats <- function(repeatFile){
 
+  ## Load required packages ##
+  CRISPRclassify:::loadRequires()
+
   ## Check if file exists ##
   CRISPRclassify:::checkFileExists(repeatFile)
 
@@ -213,6 +219,9 @@ classifyRepeats <- function(repeatFile){
 #' classifyFasta('/path/to/file.fasta')
 #' @export
 classifyFasta <- function(fastaFile){
+
+  ## Load required packages ##
+  CRISPRclassify:::loadRequires()
 
   ## Check if file exists ##
   CRISPRclassify:::checkFileExists(fastaFile)
